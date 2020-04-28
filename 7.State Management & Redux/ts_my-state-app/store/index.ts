60e1840b1b5866593e98998d9a95a5013bde716f
Damoness <qjwdcool@qq.com>
2020-04-28 18:54:20 +0800

    redux-devtools-extension

--- 7.State Management & Redux/ts_my-state-app/store/index.ts
+++ 7.State Management & Redux/ts_my-state-app/store/index.ts
@@ -1,6 +1,7 @@
 
 
 import {createStore, combineReducers} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import {mealReducer} from './meal/reducers'
 import {MealState} from './meal/types'
 
@@ -11,6 +12,6 @@ export type RootState = {
 
 const store = createStore(combineReducers({
     meal:mealReducer
-}))
+}),__DEV__ && composeWithDevTools())
 
 export default store;
